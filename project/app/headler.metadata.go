8f2c9fa427feee2ecdb76a5d54bdd856d7fc2a56
谢益文 <wsxieywien@outlook.com>
2018-03-16 09:39:16 +0800

    代码迁移

--- project/app/headler.metadata.go
+++ project/app/headler.metadata.go
@@ -2,6 +2,7 @@ package app
 
 import (
 	"fmt"
+	"time"
 	kts "project/types"
 	"github.com/ethereum/go-xethereum/crypto"
 )
@@ -11,23 +12,48 @@ func GenContentHash(content string) string {
 	return kts.Hasher([]byte(content))
 }
 
-//SignForMetadata
-func SignForContent(title string,) string {
-	
-}
-
-func GenerateMetadataFromContent(pubKey string,content string,title string,contentType string,source string,extra,data,license *kes.License){
-	contentHash = GenContentHash(content)
-	md := &kts.Metadata{}
-
-	md.DNA = GenerateDNA()
-
+//GenMetadataSignature
+func GenMetadataSignature(private_key string,title string,content_hash string,license *kts.License) string {
+	if private_key == nil || license == nil {
+		//todo : 抛出错误
+		return;
+	}
+	p, _ := crypto.ToECDSA(private_key)
+	hash_string := hex.EncodeToString(crypto.Keccak256(title,content_hash,license.Dumps()))
+	return crypto.Sign(hash_string, p)
 }
 
 func GenerateDNA(metadataSignature string) string {
 	d, _ := hex.DecodeString(metadataSignature)
 	digest := base36.EncodeBytes(crypto.Keccak256(d))
 	return digest
+}
+
+func GenerateMetadataFromContent(private_key string,md *kts.Metadata){
+	if md == nil {
+		//todo : 抛出错误
+		return;
+	}
+	contentHash = GenContentHash(md.content)
+
+	if md.PubKey == nil {
+		//todo: 应该抛出错误
+		return;
+	}
+	md.Signature = GenMetadataSignature(private_key,md.Title,contentHash,md.License)
+
+
+	//todo :　做一些非空检查 和 字段填充
+	md.Category = category
+	md.Title = title
+	md.Content = content
+	md.ContentHash = contentHash
+	md.Type = contentType
+
+	md.Created = time.Now().Unix()
+	//md.Abstract =
+	md.DNA = GenerateDNA()
+
 }
 
 
